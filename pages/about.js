180a7be6e5a4d58c3a2a47cc9c89a6a553f6cc65
Alexandr Smirnov <alexsm.dev@gmail.com>
2022-10-08 14:05:46 +0300

    add meta tags

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Router from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function About() {
 
     const handleClick = () => Router.push('/')
 
     return <>
+        <Head>
+            <title>About Page</title>
+        </Head>
+
         About page
         <button onClick={handleClick}>Home Page Button</button>
         <Link href={'/'}>Home Page Link</Link>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Link from "next/link"
+import Head from "next/head";
 
 export default function Index() {
     return (
         <>
+            <Head>
+                <title>Pet-project next.js</title>
+            </Head>
+
             <Link  href="/about"><a>About</a></Link>
             <a href="/posts">Posts</a>
             <Link  href="history//details">Details</Link>
