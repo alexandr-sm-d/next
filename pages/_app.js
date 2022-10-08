f4662d7ba4e677c54a50f6a867d27d8608a77805
Alexandr Smirnov <alexsm.dev@gmail.com>
2022-10-08 16:09:04 +0300

    use main.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 import React from "react";
+import '../styles/main.css'
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
             <Component {...pageProps} />
-            <style jsx global>{`
-                body {
-                    font-family: Roboto, sans-serif;
-                }
-            `}</style>
         </>
     )
 }
