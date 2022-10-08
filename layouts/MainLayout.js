be2b7e5cfe61e8bf9bd6150aed687d49289d3e05
Alexandr Smirnov <alexsm.dev@gmail.com>
2022-10-08 14:37:45 +0300

    add styles as html tag

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -5,13 +5,31 @@ export default function MainLayout({children}) {
     return (
         <>
             <nav>
-                <Link href={'about'}>About</Link>
-                <Link href={'posts'}>Posts</Link>
-                <Link href={'history/details'}>Details</Link>
+                <Link href={'about'}><a>About</a></Link>
+                <Link href={'posts'}><a>Posts</a></Link>
+                <Link href={'history/details'}><a>Details</a></Link>
             </nav>
             <main>
                 {children}
             </main>
+
+            <style jsx>
+                {`
+                    nav {
+                        background: green;
+                        display: flex;
+                        align-items: center;
+                        gap: 12px;
+                        height: 2rem;
+                        padding: 1rem;
+                    }
+                    
+                    nav a {
+                        color: white;
+                        text-decoration: none;
+                    }
+                `}
+            </style>
         </>
     )
 }
