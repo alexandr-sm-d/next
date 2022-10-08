055b1cb9ba953c3fd9fe2256ef3799cc7686284a
Alexandr Smirnov <alexsm.dev@gmail.com>
2022-10-08 13:49:49 +0300

    routing without reload page in browser

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Link from "next/link"
 
 export default function Index() {
     return (
         <>
-            <a href="/about">About</a>
+            <Link  href="/about"><a>About</a></Link>
             <a href="/posts">Posts</a>
-            <a href="history//details">Details</a>
+            <Link  href="history//details">Details</Link>
             <h1>Hello, Next.js!</h1>
         </>
     )
