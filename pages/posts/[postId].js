7001371158e1d349bc207068d24ca559221aa1c1
Alexandr Smirnov <alexsm.dev@gmail.com>
2022-10-20 22:46:24 +0300

    added loading for post

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,28 +1,60 @@
 import {useRouter} from "next/router";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Link from "next/link";
 import MainLayout from "../../layouts/MainLayout";
 
-export default function Post({post}) {
-    // const {query} = useRouter()
-    //
-    // console.log('query', query)
+export default function Post({serverPost}) {
+    const [frontPost, setFrontPost] = useState(serverPost)
+    const {query} = useRouter()
+
+    useEffect(() => {
+        async function load() {
+            const response = await fetch('http://localhost:4200/posts/' + query.postId)
+            const data = await response.json()
+            console.log('data', data)
+            setFrontPost(data)
+        }
+
+        // если с сервера пришел null, запускаем фронтовый запрос
+        if (!serverPost) {
+            load()
+        }
+    }, [])
+
+    if (!frontPost) {
+        return <MainLayout>
+            Loading...
+        </MainLayout>
+    }
 
     return <MainLayout>
-        <h1>{post.title}</h1>
+        <h1>{frontPost.title}</h1>
         <hr/>
         <Link href={'/posts'}><a>Back to the all posts</a></Link>
     </MainLayout>
 }
 
-Post.getInitialProps = async (ctx) => {
+Post.getInitialProps = async ({query, req}) => {
+    // генерация постов на стороне сервера
 
-    console.log('ctx post', ctx)
+    /*
+    * getInitialProps срабатывает на стороне сервера единожды
+    * тогда у него есть объект req
+    * в дальнейшем он срабатывает на стороне фронта и объект req
+    * отсутствует
+    * */
+    console.log('request object', req)
 
-    const response = await fetch('http://localhost:4200/posts/' + ctx.query.postId)
+    const isStartToFront = !req
+
+    if (isStartToFront) {
+        return {post: null}
+    }
+
+    const response = await fetch('http://localhost:4200/posts/' + query.postId)
     const post = await response.json()
 
     return {
-        post
+        serverPost: post
     }
 }
